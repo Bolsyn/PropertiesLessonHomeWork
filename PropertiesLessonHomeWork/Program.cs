3f23a398ceefb17046ec3a24692d4377f0942f24
Bolsyn <Serikagis@yandex.ru>
2020-12-28 21:13:25 +0600

    Add project files.

---
+++ PropertiesLessonHomeWork/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PropertiesLessonHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RangeOfArray ra = new RangeOfArray(-5, 5);
+
+            Console.WriteLine("Length of ra: ");
+
+            for (int i = -5; i <= 5; i++)
+                ra[i] = i;
+
+            Console.Write("Contents of ra: ");
+            for (int i = -5; i <= 5; i++)
+                Console.Write(ra[i] + " ");
+
+            Console.WriteLine("\n");
+
+        }
+    }
+}
---
+++ PropertiesLessonHomeWork/RangeOfArray.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PropertiesLessonHomeWork
+{
+    class RangeOfArray
+    {
+        public int StartOfRange { get; set; }
+        public int EndOfRange { get; set; }
+        private int[] ArrayOfNewRange;
+
+        public RangeOfArray(int start, int end)
+        {
+            end++;
+            if (end <= start)
+            {
+                Console.WriteLine("Invalid Indices");
+                end = 1;
+                start = 0;
+            }
+            ArrayOfNewRange = new int[end - start];
+
+            StartOfRange = start;
+            EndOfRange = --end;
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                return ArrayOfNewRange[index + StartOfRange];
+            }
+
+            set
+            {
+                ArrayOfNewRange[index + StartOfRange] = value;
+            }
+        }
+
+    }
+}
